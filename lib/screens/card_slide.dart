b3f6bca956d95f1f2ab66c77ffe127101353a775
joshamu20@gmail.com <joshamu20@gmail.com>
2023-03-04 23:58:15 +0100

    card slide

---
+++ lib/screens/card_slide.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:ondemand/screens/homescreen.dart';
+
+class CardCarousel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: PageView(
+        children: [
+          _buildCard("Image 1", "Description 1", "asset/banner.png"),
+          _buildCard("Image 2", "Description 2", "asset/banner.png"),
+          _buildCard("Image 3", "Description 3", "asset/banner.png"),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(String title, String subtitle, String image) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Image.asset(
+              image,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title),
+                SizedBox(height: 4),
+                Text(subtitle),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ondemand/screens/auth/signup.dart';
+import 'package:ondemand/screens/card_slide.dart';
 class homescreen extends StatefulWidget {
   const homescreen({Key? key}) : super(key: key);
 
@@ -11,27 +12,38 @@ class _homescreenState extends State<homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  body: SafeArea(
+    child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  GestureDetector(
+                    onTap: () {
+                      // handle profile image click
+                    },
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage("asset/lol.png"),
+                      radius: 24,
+                    ),
+                  ),
+                  SizedBox(width: 12),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         "Welcome\nEzekiel,",
                         style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 18,
-                            ),
+                          color: Colors.grey,
+                          fontSize: 18,
+                        ),
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
+                      SizedBox(height: 10),
                       // Text(
                       //   "Good evening",
                       //   style:
@@ -39,51 +51,65 @@ class _homescreenState extends State<homescreen> {
                       // )
                     ],
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Color(0xffF4F4F4),
-                        borderRadius: BorderRadius.circular(12)),
-                    child: Padding(
-                      padding: const EdgeInsets.all(12),
-                      child: Icon(
-                        Icons.notifications,
-                        color: Colors.purple,
-                      ),
-                    ),
-                  ),
                 ],
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 370,
-              decoration: BoxDecoration(
+              Container(
+                decoration: BoxDecoration(
                   color: Color(0xffF4F4F4),
-                  borderRadius: BorderRadius.circular(12)),
-              padding: EdgeInsets.all(12),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.search,
-                    color: Colors.grey,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Icon(
+                    Icons.notifications,
+                    color: Colors.purple,
                   ),
-                  Text(
-                    "Search for a service",
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-
-
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+        SizedBox(
+          height: 10,
+        ),
+
+        Container(
+  width: 370,
+  decoration: BoxDecoration(
+    color: Color(0xffF4F4F4),
+    borderRadius: BorderRadius.circular(12),
+  ),
+  padding: EdgeInsets.all(12),
+  child: GestureDetector(
+    onTap: () {
+      // Handle the search bar click event
+      print("Search bar clicked");
+    },
+    child: Row(
+      children: [
+        Icon(
+          Icons.search,
+          color: Colors.grey,
+        ),
+        Text(
+          "Search for a service",
+          style: TextStyle(color: Colors.grey),
+        )
+      ],
+    ),
+  ),
+),
+
+
+
+
+        SizedBox(
+          height: 20,
+        ),
+      ],
+    ),
+  ),
+);
+
   }
 }
